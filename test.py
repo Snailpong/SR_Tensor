1cecd2055792e403cfc28f83dbc20d23d29c5060
Snailpong <tjdtnsu@gmail.com>
2020-11-26 10:22:29 +0900

    flip

--- test.py
+++ test.py
@@ -16,31 +16,61 @@ from filter_func import *
 from get_lr import *
 from hashtable import *
 from matrix_compute import *
+from train import load_kmeans_model, get_features
 
-from skimage.measure import compare_psnr, compare_ssim
-
-C.argument_parse()
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 
 
-def make_image(im_LR, im_GX, im_GY, im_GZ, w, b_trace, b_fa, b_mode, h):
+def make_image(im_LR, im_GX, im_GY, im_GZ, w, kmeans, h):
     H = im_LR.shape[0]
     result_image = im_LR.copy()
-    # im_LR = np.array(im_LR, dtype=np.float64)
 
     timer = time.time()
     for i1 in range(C.PATCH_HALF, H - C.PATCH_HALF):
         print('\r{} / {}    {} s'.format(i1, H - C.PATCH_HALF, ((time.time() - timer) * 100 // 10) / 10), end='')
         timer = time.time()
-        result_image = make_image_yz(i1, result_image, im_LR, im_GX, im_GY, im_GZ, w, b_trace, b_fa, b_mode, h)
+        fS, iS = get_feature_yz(i1, result_image, im_LR, im_GX, im_GY, im_GZ, w)
+
+        if len(fS) == 0:
+            continue
+
+        jS = kmeans.predict(fS)
+        result_image = make_hr_yz(i1, result_image, im_LR, jS, h, iS)
 
     result_image = np.clip(result_image, 0, 1)
 
     return result_image
 
 
-@njit
-def make_image_yz(i1, result_image, im_LR, im_GX, im_GY, im_GZ, w, b_trace, b_fa, b_mode, h):
+def get_feature_yz(i1, result_image, im_LR, im_GX, im_GY, im_GZ, w):
     H, W, D = im_LR.shape
+    fS = []
+    iS = []
+    
+    for j1 in range(C.PATCH_HALF, W - C.PATCH_HALF):
+        for k1 in range(C.PATCH_HALF, D - C.PATCH_HALF):
+            if im_LR[i1, j1, k1] == 0:
+                continue
+
+            idxg = (slice(i1 - C.GRADIENT_HALF, i1 + C.GRADIENT_HALF + 1),
+                    slice(j1 - C.GRADIENT_HALF, j1 + C.GRADIENT_HALF + 1),
+                    slice(k1 - C.GRADIENT_HALF, k1 + C.GRADIENT_HALF + 1))
+
+            patchX = im_GX[idxg]
+            patchY = im_GY[idxg]
+            patchZ = im_GZ[idxg]
+
+            features = get_features(patchX, patchY, patchZ, w)
+            fS.append(features[:-2])
+            iS.append(features[-2:])
+
+    return fS, iS
+
+
+# @jit
+def make_hr_yz(i1, result_image, im_LR, jS, h, iS):
+    H, W, D = im_LR.shape
+    cnt = 0
     
     for j1 in range(C.PATCH_HALF, W - C.PATCH_HALF):
         for k1 in range(C.PATCH_HALF, D - C.PATCH_HALF):
@@ -52,95 +82,73 @@ def make_image_yz(i1, result_image, im_LR, im_GX, im_GY, im_GZ, w, b_trace, b_fa
             if im_LR[i1, j1, k1] == 0:
                 continue
 
-            # np.where(patch == 0, patch[C.PATCH_HALF, C.PATCH_HALF, C.PATCH_HALF], patch)
-
-            # if np.any(patch == 0):
-            #     patch[np.where(patch == 0)] = patch[PATCH_HALF, PATCH_HALF, PATCH_HALF]
-
-            idxg = (slice(i1 - C.GRADIENT_HALF, i1 + C.GRADIENT_HALF + 1),
-                    slice(j1 - C.GRADIENT_HALF, j1 + C.GRADIENT_HALF + 1),
-                    slice(k1 - C.GRADIENT_HALF, k1 + C.GRADIENT_HALF + 1))
-
-            # patch_std = patch.std()
-            patchX = im_GX[idxg]
-            patchY = im_GY[idxg]
-            patchZ = im_GZ[idxg]
-
-            angle_p, angle_t, trace, fa, mode = get_hash(patchX, patchY, patchZ, w, b_trace, b_fa, b_mode)
-            j = int(angle_p * C.Q_TRACE * C.Q_FA * C.Q_MODE * C.Q_ANGLE_T + angle_t * C.Q_TRACE * C.Q_FA * C.Q_MODE + trace * C.Q_FA * C.Q_MODE + fa * C.Q_MODE + mode)
-
             # patch1 = patch.ravel()
+            # print(iS[cnt][0])
+            patch = np.transpose(patch, iS[cnt][0])
+
+            if iS[cnt][1][0] > 0 and iS[cnt][1][1] < 0:
+                patch = np.flip(patch, axis=2)
+            elif iS[cnt][1][0] < 0 and iS[cnt][1][1] > 0:
+                patch = np.flip(patch, axis=1)
+            elif iS[cnt][1][0] < 0 and iS[cnt][1][1] < 0:
+                patch = np.flip(patch, axis=0)
+
             patch1 = np.append(patch, 1).astype(np.float32)
-            # patch1.append(1)
-            result_image[i1, j1, k1] = np.dot(patch1, h[j])
+            result_image[i1, j1, k1] = np.dot(patch1, h[jS[cnt]])
+            cnt += 1
 
     return result_image
 
 
-current_hour = time.strftime('%m%d%H', time.localtime(time.time()))
-result_dir = './result/{}/'.format(current_hour)
-if not os.path.isdir(result_dir):
-    os.mkdir(result_dir)
+if __name__ == '__main__':
+    C.argument_parse()
+    C.Q_TOTAL = 512
 
-file_list = make_dataset('../NISR/test')
+    current_hour = time.strftime('%m%d%H', time.localtime(time.time()))
+    result_dir = './result/{}_{}x_{}/'.format(current_hour, C.R, C.Q_TOTAL)
+    if not os.path.isdir(result_dir):
+        os.mkdir(result_dir)
 
-# Preprocessing normalized Gaussian matrix W for hashkey calculation
-G_WEIGHT = get_normalized_gaussian()
+    file_list = make_dataset('../NISR/test')
 
-h = np.load('./arrays/h_{}.npy'.format(C.R))
+    # Preprocessing normalized Gaussian matrix W for hashkey calculation
+    G_WEIGHT = get_normalized_gaussian()
 
-# cnt = 0
-# for i in range(h.shape[0]):
-#     if h[i].max() >= 10 or h[i].min() <= -10:
-#         h[i] = np.zeros((h.shape[1]))
-#         h[i, C.PATCH_HALF * (C.PATCH_SIZE ** 2 + C.PATCH_SIZE) + C.PATCH_HALF] = 1
-#         cnt += 1
-# print(cnt)
+    h = np.load('./arrays/h_{}x_{}.npy'.format(C.R, C.Q_TOTAL))
+    kmeans = load_kmeans_model()
 
-with open("./arrays/Qfactor_" + str(C.R), "rb") as p:
-    b_trace, b_fa, b_mode = pickle.load(p)
-
-print(b_trace, b_fa, b_mode)
-filestart = time.time()
-
-for file_idx, file in enumerate(file_list):
-    file_name = file.split('\\')[-1].split('.')[0]
-    print('\r', end='')
-    print('' * 60, end='')
-    print('\rProcessing ' + str(file_idx + 1) + '/' + str(len(file_list)) + ' image (' + file_name + ')' + str(time.time() - filestart))
     filestart = time.time()
 
-    raw_image = np.array(nib.load(file).get_fdata(), dtype=np.float32)
-    clipped_image = clip_image(raw_image)
-    im = mod_crop(clipped_image, C.R)
-    slice_area = crop_slice(im, C.PATCH_SIZE // 2, C.R)
+    for file_idx, file in enumerate(file_list):
+        file_name = file.split('\\')[-1].split('.')[0]
+        print('\r', end='')
+        print('' * 60, end='')
+        print('\rProcessing ' + str(file_idx + 1) + '/' + str(len(file_list)) + ' image (' + file_name + ')' + str(time.time() - filestart))
+        filestart = time.time()
 
-    im_HR = im[slice_area] / im.max()
-    im_blank_LR = get_lr(im) / im.max()  # Prepare the cheap-upscaling images
-    im_LR = im_blank_LR[slice_area]
-    im_GX, im_GY, im_GZ = np.gradient(im_LR)  # Calculate the gradient images
+        raw_image = np.array(nib.load(file).get_fdata(), dtype=np.float32)
+        clipped_image = clip_image(raw_image)
+        im = mod_crop(clipped_image, C.R)
+        slice_area = crop_slice(im, C.PATCH_SIZE // 2, C.R)
 
-    im_result = make_image(im_LR, im_GX, im_GY, im_GZ, G_WEIGHT, b_trace, b_fa, b_mode, h)
+        im_HR = im[slice_area] / im.max()
+        im_blank_LR = get_lr(im) / im.max()
+        im_LR = im_blank_LR[slice_area]
+        im_GX, im_GY, im_GZ = np.gradient(im_LR)
 
-    output_img = np.zeros(raw_image.shape)
-    output_img[slice_area] = im_result
-    output_img = output_img * im.max()
-    ni_img = nib.Nifti1Image(output_img, np.eye(4))
-    nib.save(ni_img, '{}/{}_result.nii.gz'.format(result_dir, file_name))
+        im_result = make_image(im_LR, im_GX, im_GY, im_GZ, G_WEIGHT, kmeans, h)
 
-    # output_img2 = np.zeros(raw_image.shape)
-    # output_img2[slice_area] = im_blending
-    # output_img2 = output_img2 * clipped_image.max()
-    # ni_img2 = nib.Nifti1Image(output_img2, np.eye(4))
-    # nib.save(ni_img2, '{}/{}_result_blend.nii.gz'.format(result_dir, file_name))
+        output_img = np.zeros(raw_image.shape)
+        output_img[slice_area] = im_result
+        output_img = output_img * im.max()
+        ni_img = nib.Nifti1Image(output_img, np.eye(4))
+        nib.save(ni_img, '{}/{}_result.nii.gz'.format(result_dir, file_name))
 
-    print()
-    print(compare_psnr(im_HR, im_LR), compare_psnr(im_HR, im_result))
-    print(compare_ssim(im_HR, im_LR), compare_ssim(im_HR, im_result))
+        print()
+        print(peak_signal_noise_ratio(im_HR, im_LR), peak_signal_noise_ratio(im_HR, im_result))
+        print(structural_similarity(im_HR, im_LR), structural_similarity(im_HR, im_result))
 
-    #if file_idx == 0:
-        #break
+        if file_idx == 0:
+            break
 
-
-
-print("Test is off")
+    print("Test is off")
--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 import glob
 import time
 import random
+import math
 
 import cupy as cp
 import numpy as np
 
 import nibabel as nib
+from sklearn.cluster import KMeans
 
 import filter_constant as C
 
@@ -16,96 +18,258 @@ from hashtable import *
 from matrix_compute import *
 from util import *
 
-C.argument_parse()
 
-# Q, V, finished_files, count = load_files()
-Q = np.zeros((C.Q_TOTAL, C.FILTER_VOL+1, C.FILTER_VOL+1), dtype=np.float64)
-V = np.zeros((C.Q_TOTAL, C.FILTER_VOL+1), dtype=np.float64)
-finished_files = []
-count = np.zeros(C.Q_TOTAL, dtype=int)
+@njit
+def get_features(patchX, patchY, patchZ, weight):
+    G = np.vstack((patchX.ravel(), patchY.ravel(), patchZ.ravel())).T
+    x = G.T @ weight @ G
+    w, v = np.linalg.eig(x)
 
-b_trace = np.zeros((C.Q_TRACE - 1)) 
-b_fa = np.zeros((C.Q_FA - 1)) 
-b_mode = np.zeros((C.Q_MODE - 1)) 
+    index = w.argsort()[::-1]
+    [l1, l2, l3] = w[index]
+    v = v[:, index]
 
-file_list = make_dataset('../NISR/train')
-C.TRAIN_FILE_MAX = min(C.TRAIN_FILE_MAX, len(file_list))
+    v1 = v[:, 0]
+    index1 = np.abs(v1).argsort()[::-1]
+    v1 = v1[index1]
+    sign = np.sign(v1[1:])
+    v1[1:] = np.abs(v1[1:])
 
-# Preprocessing normalized Gaussian matrix W for hashkey calculation
-G_WEIGHT = get_normalized_gaussian()
+    angle_p = math.atan2(v1[1], v1[0])
+    angle_t = math.acos(v1[2] / (sqrt((v1[0]) ** 2 + v1[1] ** 2 + v1[2] ** 2) + 1e-16))
 
-# instance = 5000000
-# patchNumber = 0
-# quantization = np.zeros((instance, 3))
-# for file_idx, image in enumerate(file_list):
-#     print('\r', end='')
-#     print('' * 60, end='')
-#     print('\r Quantization: Processing '+ image.split('\\')[-1] + str(instance) + ' patches (' + str(100*patchNumber/instance) + '%)')
+    trace, fa, mode = get_lamda_u(l1, l2, l3)
 
-#     raw_image = np.array(nib.load(image).get_fdata(), dtype=np.float32)
-#     clipped_image = clip_image(raw_image)
-#     im = mod_crop(clipped_image, C.R)
+    # return angle_p, angle_t, math.log(trace), fa, mode
+    return angle_p, angle_t, math.log(trace), fa, index1, sign
 
-#     slice_area = crop_slice(im, C.PATCH_SIZE // 2, C.R)
-#     im_LR = get_lr(im)
 
-#     im_blank_LR = get_lr(im) / im.max()
-#     im_LR = im_blank_LR[slice_area]
-#     im_GX, im_GY, im_GZ = np.gradient(im_LR)  # Calculate the gradient images
+@njit
+def make_point_space(im_LR, im_GX, im_GY, im_GZ, patchNumber, w, point_space, MAX_POINTS):
+    H, W, D = im_GX.shape
 
-#     quantization, patchNumber = quantization_border(im_LR, im_GX, im_GY, im_GZ, patchNumber, G_WEIGHT, quantization, instance)  # get the strength and coherence of each patch
-#     if patchNumber > instance / 2:
-#         break
+    for i1 in range(C.PATCH_HALF, H - C.PATCH_HALF):
+        # print(i1)
+        for j1 in range(C.PATCH_HALF, W - C.PATCH_HALF):
+            for k1 in range(C.PATCH_HALF, D - C.PATCH_HALF):
 
-# # uniform quantization of patches, get the optimized strength and coherence boundaries
-# quantization = quantization[0:patchNumber, :]
-# quantization = np.sort(quantization, axis=0)
+                # if random.random() > 0.2 or np.any(im_LR[i1, j1, k1] == 0):
+                #     continue
 
-# for i in range(C.Q_TRACE - 1):
-#     b_trace[i] = quantization[floor((i+1) * patchNumber / C.Q_TRACE), 0]
-#     b_fa[i] = quantization[floor((i+1) * patchNumber / C.Q_FA), 1]
-#     b_mode[i] = quantization[floor((i+1) * patchNumber / C.Q_MODE), 2]
+                if im_LR[i1, j1, k1] == 0:
+                    continue
 
-b_trace = np.array([0.03283301, 0.05263998])
-b_fa = np.array([0.36205414, 0.47029916])
-b_mode = np.array([0.84320635, 0.96727021])
+                idxg = (slice(i1 - C.GRADIENT_HALF, i1 + C.GRADIENT_HALF + 1),
+                        slice(j1 - C.GRADIENT_HALF, j1 + C.GRADIENT_HALF + 1),
+                        slice(k1 - C.GRADIENT_HALF, k1 + C.GRADIENT_HALF + 1))
 
-# b_trace = np.array([0.05, 0.15])
-# b_fa = np.array([0.35, 0.45])
-# b_mode = np.array([0, 0.75])
+                patchX = im_GX[idxg]
+                patchY = im_GY[idxg]
+                patchZ = im_GZ[idxg]
 
-print(b_trace, b_fa, b_mode)
+                point_space[patchNumber] = np.array(get_features(patchX, patchY, patchZ, w)[:-2])
+                patchNumber += 1
 
-start = time.time()
+                # print(point_space[patchNumber-1, 0:2])
 
-for file_idx, file in enumerate(file_list):
-    file_name = file.split('\\')[-1].split('.')[0]
-    filestart = time.time()
+    return point_space, patchNumber
 
-    if file_idx >= 100:
-        break
 
-    if file in finished_files:
-        continue
+def k_means_modeling(point_space, patchNumber, n_clusters):
+    quantization = point_space[0:patchNumber, :]
+    kmeans = KMeans(n_clusters=n_clusters, verbose=True, max_iter=30, n_init=1)
+    kmeans.fit(quantization)
 
-    print('\rProcessing ' + str(file_idx + 1) + '/' + str(len(file_list)) + ' image (' + file_name + ')')
+    return kmeans
 
-    raw_image = np.array(nib.load(file).get_fdata(), dtype=np.float32)
-    clipped_image = clip_image(raw_image)
-    im = mod_crop(clipped_image, C.R)
-    slice_area = crop_slice(im, C.PATCH_HALF, C.R)
 
-    im_blank_LR = get_lr(im) / im.max()  # Prepare the cheap-upscaling images
-    im_LR = im_blank_LR[slice_area]
-    im_HR = im[slice_area] / im.max()
+def train_qv2(im_LR, im_HR, w, kmeans, Q, V, count):
+    H, W, D = im_HR.shape
+    im_GX, im_GY, im_GZ = np.gradient(im_LR)  # Calculate the gradient images
 
-    Q, V, count = train_qv2(im_LR, im_HR, G_WEIGHT, b_trace, b_fa, b_mode, Q, V, count)  # get Q, V of each patch
-    
-    print(' ' * 30, 'last', '%.1f' % ((time.time() - filestart) / 60), 'min', end='', flush=True)
+    xyz_range = [[x, y, z] for x in range(C.PATCH_HALF, H - C.PATCH_HALF)
+                    for y in range(C.PATCH_HALF, W - C.PATCH_HALF)
+                    for z in range(C.PATCH_HALF, D - C.PATCH_HALF)]
+    sample_range = random.sample(xyz_range, len(xyz_range) // C.SAMPLE_RATE)
+    point_list = chunk(sample_range, len(sample_range) // C.TRAIN_DIV + 1)
 
-    finished_files.append(file)
+    for sample_idx, point_list1 in enumerate(point_list):
+        print('\r{} / {}'.format(sample_idx + 1, len(point_list)), end='', flush=True)
+        patchS, xS = init_buckets()
+        fS = []
+        iS = []
 
-compute_h(Q, V)
+        timer = time.time()
 
-with open("./arrays/Qfactor_" + str(C.R), "wb") as p:
-    pickle.dump([b_trace, b_fa, b_mode], p)
+        for i1, j1, k1 in point_list1:
+            if im_HR[i1, j1, k1] == 0:
+                continue
+
+            idxg = (slice(i1 - C.GRADIENT_HALF, i1 + C.GRADIENT_HALF + 1),
+                    slice(j1 - C.GRADIENT_HALF, j1 + C.GRADIENT_HALF + 1),
+                    slice(k1 - C.GRADIENT_HALF, k1 + C.GRADIENT_HALF + 1))
+
+            patchX = im_GX[idxg]
+            patchY = im_GY[idxg]
+            patchZ = im_GZ[idxg]
+
+            features = get_features(patchX, patchY, patchZ, w)
+            fS.append(features[:-2])
+            iS.append(features[-2:])
+
+        print('   get_feature {} s'.format(((time.time() - timer) * 1000 // 10) / 100), end='', flush=True)
+        timer = time.time()
+
+        jS = kmeans.predict(fS)
+        cnt = 0
+
+        print('   predict {} s'.format(((time.time() - timer) * 1000 // 10) / 100), end='', flush=True)
+        timer = time.time()
+
+        for i1, j1, k1 in point_list1:
+
+            if im_HR[i1, j1, k1] == 0:
+                continue
+
+            idxp = (slice(i1 - C.PATCH_HALF, i1 + C.PATCH_HALF + 1),
+                    slice(j1 - C.PATCH_HALF, j1 + C.PATCH_HALF + 1),
+                    slice(k1 - C.PATCH_HALF, k1 + C.PATCH_HALF + 1))
+
+            patch = im_LR[idxp]
+            # print(iS[cnt])
+            patch = np.transpose(patch, iS[cnt][0])
+
+            if iS[cnt][1][0] > 0 and iS[cnt][1][1] < 0:
+                patch = np.flip(patch, axis=2)
+            elif iS[cnt][1][0] < 0 and iS[cnt][1][1] > 0:
+                patch = np.flip(patch, axis=1)
+            elif iS[cnt][1][0] < 0 and iS[cnt][1][1] < 0:
+                patch = np.flip(patch, axis=0)
+
+            # patch1 = patch.reshape(-1) 
+            patch1 = np.append(patch, 1)
+            x1 = im_HR[i1, j1, k1]
+
+            patchS[jS[cnt]].append(patch1)
+            xS[jS[cnt]].append(x1)
+            count[jS[cnt]] += 1
+            cnt += 1
+
+        print('   append {} s'.format(((time.time() - timer) * 1000 // 10) / 100), end='', flush=True)
+        timer = time.time()
+
+        for j in range(C.Q_TOTAL):
+            if len(xS[j]) != 0:
+                A = np.array(patchS[j])
+                b = np.array(xS[j]).reshape(-1, 1)
+                Q[j] += np.dot(A.T, A)
+                V[j] += np.dot(A.T, b).reshape(-1)
+
+        print('   qv {} s'.format((time.time() - timer) * 100 // 10 / 10), end='', flush=True)
+
+    return Q, V, count
+
+
+def make_kmeans_model():
+    G_WEIGHT = get_normalized_gaussian()
+
+    MAX_POINTS = 15000000
+    patchNumber = 0
+    point_space = np.zeros((MAX_POINTS, 4))
+
+    for file_idx, image in enumerate(file_list):
+        print('\r', end='')
+        print('' * 60, end='')
+        print('\r Making Point Space: '+ image.split('\\')[-1] + str(MAX_POINTS) + ' patches (' + str(100*patchNumber/MAX_POINTS) + '%)')
+
+        raw_image = np.array(nib.load(image).get_fdata(), dtype=np.float32)
+        clipped_image = clip_image(raw_image)
+        im = mod_crop(clipped_image, C.R)
+
+        slice_area = crop_slice(im, C.PATCH_SIZE // 2, C.R)
+        im_LR = get_lr(im)
+
+        im_blank_LR = get_lr(im) / im.max()
+        im_LR = im_blank_LR[slice_area]
+        im_GX, im_GY, im_GZ = np.gradient(im_LR)
+
+        point_space, patchNumber = make_point_space(im_LR, im_GX, im_GY, im_GZ, patchNumber, G_WEIGHT, point_space, MAX_POINTS)
+        if patchNumber > MAX_POINTS / 2:
+            break
+
+    start = time.time()
+    print('start clustering')
+    kmeans = k_means_modeling(point_space, patchNumber, C.Q_TOTAL)
+    print(time.time() - start)
+
+    with open('./arrays/space_{}x_{}.km'.format(C.R, C.Q_TOTAL), 'wb') as p:
+        pickle.dump(kmeans, p)
+
+    return kmeans
+
+
+def load_kmeans_model():
+    with open('./arrays/space_{}x_{}.km'.format(C.R, C.Q_TOTAL), "rb") as p:
+        kmeans = pickle.load(p)
+    return kmeans
+
+
+if __name__ == '__main__':
+    C.argument_parse()
+    C.Q_TOTAL = 512
+
+    Q = np.zeros((C.Q_TOTAL, C.FILTER_VOL+1, C.FILTER_VOL+1), dtype=np.float64)
+    V = np.zeros((C.Q_TOTAL, C.FILTER_VOL+1), dtype=np.float64)
+    finished_files = []
+    count = np.zeros(C.Q_TOTAL, dtype=int)
+
+    file_list = make_dataset('../NISR/train')
+    C.TRAIN_FILE_MAX = min(C.TRAIN_FILE_MAX, len(file_list))
+
+    # Preprocessing normalized Gaussian matrix W for hashkey calculation
+    G_WEIGHT = get_normalized_gaussian()
+
+    kmeans = make_kmeans_model()
+    # kmeans = load_kmeans_model()
+
+    start = time.time()
+
+    for file_idx, file in enumerate(file_list):
+        file_name = file.split('\\')[-1].split('.')[0]
+        filestart = time.time()
+
+        if file_idx >= 100:
+            break
+
+        if file in finished_files:
+            continue
+
+        print('\rProcessing ' + str(file_idx + 1) + '/' + str(len(file_list)) + ' image (' + file_name + ')')
+
+        raw_image = np.array(nib.load(file).get_fdata(), dtype=np.float32)
+        clipped_image = clip_image(raw_image)
+        im = mod_crop(clipped_image, C.R)
+        slice_area = crop_slice(im, C.PATCH_HALF, C.R)
+
+        im_blank_LR = get_lr(im) / im.max()
+        im_LR = im_blank_LR[slice_area]
+        im_HR = im[slice_area] / im.max()
+
+        Q, V, count = train_qv2(im_LR, im_HR, G_WEIGHT, kmeans, Q, V, count)
+        
+        print(' ' * 5, 'last', '%.1f' % ((time.time() - filestart) / 60), 'min', end='', flush=True)
+
+        finished_files.append(file)
+
+    # compute_h(Q, V)
+
+    h = np.zeros((Q.shape[0], Q.shape[1]))
+
+    print("\rComputing H...   ")
+    start = time.time()
+    for j in range(C.Q_TOTAL):
+        print('\r{} / {}'.format(j + 1, C.Q_TOTAL), end='')
+        h[j] = cg(Q[j], V[j], tol=1e-5)[0]
+
+    h = np.array(h, dtype=np.float32)
+    np.save('./arrays/h_{}x_{}'.format(C.R, C.Q_TOTAL), h)
